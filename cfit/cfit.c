999a36d79ed332ee4b93a6ce301a04bc3a7ba724
steinhh <s.v.h.haugan@astro.uio.no>
2025-06-17 21:52:54 +0200

    [ cfit.* ] WIP: adding cf_g_p0_ but understanding more about temp vs. perm. variables... I can't make them!

--- cfit/cfit.c
+++ cfit/cfit.c
@@ -26,7 +26,7 @@ void assert_numeric(const char *description, IDL_VPTR arg)
   }
 }
 
-void check_params(int argc, IDL_VPTR Argv[])
+void check_numeric_array_params(int argc, IDL_VPTR Argv[])
 {
   IDL_ENSURE_ARRAY(Argv[0]);
   IDL_ENSURE_ARRAY(Argv[1]);
@@ -37,10 +37,10 @@ void check_params(int argc, IDL_VPTR Argv[])
   assert_numeric("A (coefficients)", Argv[1]);
 }
 
-void make_permanent_arr_from_template(IDL_VPTR x_vptr, IDL_VPTR dest)
+void make_arr_from_template(IDL_VPTR x_vptr, IDL_VPTR dest)
 {
   IDL_VPTR tmp;
-  // First temporary then make permanent with IDL_VarCopy
+  // First temporary then copy it where caller wants
   IDL_VarMakeTempFromTemplate(x_vptr, x_vptr->type, 0 /*not struct*/, &tmp, FALSE);
   IDL_VarCopy(tmp, dest);
 }
@@ -63,15 +63,15 @@ void make_pder_array(IDL_VPTR x_vptr, IDL_VPTR a_vptr, IDL_VPTR pder, IDL_MEMINT
 /*; Use         : COMP_POLY,X,A,F [,PDER]*/
 static void COMP_POLY(int argc, IDL_VPTR Argv[], char *argk)
 {
-  check_params(argc, Argv);
+  check_numeric_array_params(argc, Argv);
 
   IDL_VPTR x_vptr = IDL_CvtDbl(1, Argv);
   IDL_VPTR a_vptr = IDL_CvtDbl(1, Argv + 1);
 
   IDL_VPTR f_vptr = Argv[2]; /* Output array */
-  IDL_VPTR pder_vptr = argc > 3 ? Argv[3] : NULL;
+  IDL_VPTR pder_vptr = argc > 3 ? Argv[3] : NULL; // Partial derivatives, optional
 
-  make_permanent_arr_from_template(x_vptr, f_vptr);
+  make_arr_from_template(x_vptr, f_vptr);
 
   double *x = (void *) x_vptr->value.arr->data;
   double *a = (void *) a_vptr->value.arr->data;
@@ -108,14 +108,14 @@ static void COMP_GAUSS(int argc, IDL_VPTR Argv[], char *argk)
 {
   char msg[256];
   info("COMP_GAUSS: Running!");
-  check_params(argc, Argv);
+  check_numeric_array_params(argc, Argv);
 
-  IDL_VPTR x_vptr = IDL_CvtDbl(1, Argv); /* Input array */
-  IDL_VPTR a_vptr = IDL_CvtDbl(1, Argv + 1); /* Coefficients */
+  IDL_VPTR x_vptr = IDL_CvtDbl(1, Argv);
+  IDL_VPTR a_vptr = IDL_CvtDbl(1, Argv + 1);
   IDL_VPTR f_vptr = Argv[2]; /* Output array */
-  IDL_VPTR pder_vptr = argc > 3 ? Argv[3] : NULL; /* Partial derivatives, optional */
+  IDL_VPTR pder_vptr = argc > 3 ? Argv[3] : NULL; // Partial derivatives, optional
 
-  make_permanent_arr_from_template(x_vptr, f_vptr);
+  make_arr_from_template(x_vptr, f_vptr);
 
   double *a = (void *) a_vptr->value.arr->data;
   double *f = (void *) f_vptr->value.arr->data;
@@ -171,13 +171,74 @@ oplot,x,pder[*,1]/10.
 oplot,x,pder[*,2]/20.
 */
 
+double *make_perm_vector(IDL_VPTR vptr, IDL_LONG64 n_elts)
+{
+  IDL_VPTR tmp;
+  IDL_MakeTempArray(IDL_TYP_DOUBLE, 1, &n_elts, IDL_ARR_INI_NOP, &tmp);
+  IDL_VarCopy(tmp, vptr);
+  return (double *) vptr->value.arr->data;
+}
+
 // ****************************************************************************************************
+// ***************************************** cf_g_p0_ *************************************************
 // ****************************************************************************************************
-// ****************************************************************************************************
+
+static void cf_g_p0_(int argc, IDL_VPTR Argv[], char *argk)
+{
+  char msg[256];
+  info("cf_g_p0_: Running!");
+  check_numeric_array_params(argc, Argv);
+
+  IDL_VPTR x_vptr = IDL_CvtDbl(1, Argv);
+  IDL_VPTR a_vptr = IDL_CvtDbl(1, Argv + 1);
+  IDL_VPTR f_vptr = Argv[2]; /* Pointer to var to store output */
+  IDL_VPTR pder_vptr = argc > 3 ? Argv[3] : NULL; // Partial derivatives, optional
+
+  make_arr_from_template(x_vptr, f_vptr); // Also permanent, we'll return it
+
+  double *x = (void *) x_vptr->value.arr->data;
+  double *a = (void *) a_vptr->value.arr->data;
+  double *f = (void *) f_vptr->value.arr->data;
+  double *pder = NULL;
+
+  // Partial derivative will be [ a0, a1, a2, a3 ] where
+  // a0...a2 are gauss params, and a3 is polynomial constant
+  IDL_MEMINT pder_dim[2];
+  if (pder_vptr) {
+    make_pder_array(x_vptr, a_vptr, pder_vptr, pder_dim);
+    pder = (void *) pder_vptr->value.arr->data;
+  }
+
+  // Stage things for calling COMP_GAUSS
+  IDL_VPTR gauss_a_vptr = IDL_Gettmp(); // For gauss params
+  double *gauss_a = make_perm_vector(gauss_a_vptr, 3); // a0, a1, a2
+  gauss_a[0] = a[0]; // a0 is the height
+  gauss_a[1] = a[1]; // a1 is the center
+  gauss_a[2] = a[2]; // a2 is the width
+
+  IDL_VPTR comp_f_vptr = IDL_Gettmp(); // For receiving result f from component
+  IDL_VPTR comp_pder_vptr = pder_vptr ? IDL_Gettmp() : NULL; // For receiving pder from component
+
+  info("Here we go 7");
+
+  IDL_VPTR comp_args[4]; // For sending args to component
+  comp_args[0] = x_vptr; // Input array
+  // comp_args[1] = gauss_a_vptr; // Coefficients for gauss
+  comp_args[2] = comp_f_vptr; // Output array for gauss
+  comp_args[3] = comp_pder_vptr; // Partial derivatives for gauss, optional
+  int comp_argc = pder_vptr ? 4 : 3; // Number of args for component
+
+  // COMP_GAUSS(comp_argc, comp_args, NULL); // Call COMP_GAUSS with 3 args
+
+  // These might be temp. due to type conversion
+  IDL_DELTMP(x_vptr);
+  IDL_DELTMP(a_vptr);
+}
 
 int IDL_Load(void)
 {
   static IDL_SYSFUN_DEF2 pro_def[] = {{(IDL_SYSRTN_GENERIC) COMP_POLY, "COMP_POLY", 3, 4, 0, 0},
-                                      {(IDL_SYSRTN_GENERIC) COMP_GAUSS, "COMP_GAUSS", 3, 4, 0, 0}};
-  return IDL_SysRtnAdd(pro_def, FALSE, 2);
+                                      {(IDL_SYSRTN_GENERIC) COMP_GAUSS, "COMP_GAUSS", 3, 4, 0, 0},
+                                      {(IDL_SYSRTN_GENERIC) cf_g_p0_, "CF_G_P0_", 3, 4, 0, 0}};
+  return IDL_SysRtnAdd(pro_def, FALSE, 3);
 }
